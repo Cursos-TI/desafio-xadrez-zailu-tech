050a3229a884adcbcac2edf5ccb13a4d3b47799b
zailu-tech <Luizaabreudossantos97@gmail.com>
2025-02-17 17:48:37 +0000

    Desafio mestre concluído

--- xadrez.c
+++ xadrez.c
@@ -3,37 +3,66 @@
 // Desafio de Xadrez - MateCheck
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
   
-int main() {
-   
-        printf("Desafio Novato concluído\n");
-    int linha = 8 , coluna = 7; // Posição inicial do bispo
-    int linha_torre = 6 , coluna_torre= 4; // Posição inicial da torre
-    int linha_rainha = 3, coluna_rainha = 8; // Posição inicial da rainha
+#include <stdio.h>
+                
 
-    printf("Movimentação do Bispo a partir do movimento (%d, %d): \n", linha ,coluna);
-    for(int i = 1; i <= 5; i++) {  //  Movimento para cima e para direita até 5 casas
-        linha--; // Move para cima
-        coluna++; // Move para direita
-        printf("(%d, %d) direita, cima\n" , linha, coluna); // saída de dados
-      }
-
-      printf("Movimentação da torre a partir do movimento (%d, %d): \n", linha_torre ,coluna_torre);
-        for(int i = 1; i <= 5; i++){  //  Movimento para direita até 5 casas;
-            coluna_torre++; // Move para direita
-            printf("(%d, %d) direita\n" ,linha_torre, coluna_torre); // saía de dados
-        }
-            
-        printf("Movimentação da rainha a partir do movimento (%d, %d): \n", linha_rainha ,coluna_rainha);
-            for(int i = 1; i <= 8; i++){  // Movimento para esquerda até 8 casas
-             if(coluna_rainha <= 0){
-              printf("A rainha não pode mover para a próxima casa, pois saiu do tabuleiro.\n");
-            break;  // Parar o movimento se a coluna for 0 ou menor
-                }
-            coluna_rainha--; // Move para esquerda
-             printf("(%d, %d) esquerda\n" ,linha_rainha, coluna_rainha); // saída de dados
-            }
-             return 0;
+void rainha (int quantidade){
+    if(quantidade > 0){
+        rainha(quantidade - 1);
+        printf("%d esquerda\n", quantidade);
     }
+   }
 
+void torre (int movimento) { 
+    if(movimento > 0) {
+        torre(movimento - 1);
+        printf("%d direita\n", movimento);
+    }
+}
+
+int main(){
+    int movimento1=0;
+    int numero = 5;
+    printf("Movimento da torre a partir da casa atual (%d)\n", movimento1);
+    torre(numero);
+
+    int movimento2 = 0;
+    int movimento = 8;
+    printf("Movimento rainha a partir da casa atual(%d)\n", movimento2);
+    rainha(movimento);
+
+   int linha_bispo = 0;
+   int coluna_bispo = 0;
+   printf("Movimentação do Bispo a partir do movimento (%d, %d): \n", linha_bispo ,coluna_bispo);
+   
+   while(linha_bispo <= 5){
+    coluna_bispo = 0;
+    while(coluna_bispo <= 5){
+        if(linha_bispo == coluna_bispo)
+        printf("(%d - %d) direita, cima\n", coluna_bispo ,linha_bispo);
+        coluna_bispo++;
+    }
+    linha_bispo++;
+
+   }
+
+   int movimento3 = 0;
+   int movimento4 = 0;
+   for(int linha_cavalo = 2, coluna_cavalo = 1; linha_cavalo <=2 && coluna_cavalo <= 1; linha_cavalo++, coluna_cavalo++){
+     printf("Movimentação do cavalo a partir da casa atual (%d, %d)\n", movimento3, movimento4);
+    printf("(%d - %d) cima, cima, direita\n",linha_cavalo, coluna_cavalo);
+   }  
+    return 0;
+     }
+      
+
+
+
+
+
+  
+        
+    
+    
 
 
